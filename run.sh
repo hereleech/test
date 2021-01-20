9129867a7c5a6822f60496d4b719b35629596275
artemiszuk <68847756+artemiszuk@users.noreply.github.com>
2021-01-20 21:30:09 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-git clone https://artemiszuk:mygithub004@github.com/artemiszuk/UsergeS
-cd UsergeS
-bash run
+git clone https://artemiszuk:mygithub004@github.com/artemiszuk/atmb-pro
+cd atmb-pro
+nodeenv env
+. env/bin/activate
+npm install
+tsc
+bash aria.sh
+npm start
