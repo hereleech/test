65b4006a875002491825f8a1f635474ed59c4e71
artemiszuk <68847756+artemiszuk@users.noreply.github.com>
2021-01-20 13:13:56 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,7 +9,5 @@ aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port $RPC_LISTEN_PORT --
 git clone https://artemiszuk:mygithub004@github.com/artemiszuk/UsergeS
 cd UsergeS
 git checkout master
-virtualenv -p /usr/bin/python3 venv
-. ./venv/bin/activate
 pip3 install aiofiles aiohttp bs4 covid cowpy dnspython emoji gitpython google-api-python-client google-auth-httplib2 google-auth-oauthlib googletrans==3.1.0a0 hachoir heroku3 html-telegraph-poster instaloader pymongo motor natsort oauth2client Pillow==8.0 psutil pybase64 pyrogram==1.1.12 pySmartDL python-dotenv pytz rarfile removebg requests search-engine-parser selenium==3.141.0 setuptools>=40.3.0 spamwatch speedtest-cli stagger telegraph tgcrypto urbandict==0.5 wget wikipedia youtube_dl>=2021.1.8
 bash run
