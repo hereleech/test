df2eeab920b25a150493e72ff68b7162c18c27cc
artemiszuk <68847756+artemiszuk@users.noreply.github.com>
2021-01-23 00:01:50 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,5 @@ python3 -m venv env
 source ./env/bin/activate
 git clone https://artemiszuk:mygithub004@github.com/artemiszuk/telegram-bot
 cd telegram-bot
-pip3 install -r requirements.txt
 python3 -m tobrot
 
