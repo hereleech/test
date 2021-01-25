7c0b5b8496816707557d0361015ee6e140378e10
artemiszuk <68847756+artemiszuk@users.noreply.github.com>
2021-01-25 10:20:12 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-git clone https://artemiszuk:mygithub004@github.com/artemiszuk/fami
-cd fami
+git clone https://artemiszuk:mygithub004@github.com/artemiszuk/magneto-python-aria-jan
+cd magneto-python-aria-jan
 bash aria.sh
 python3 -m bot
 
