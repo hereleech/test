99c5045b7c9231c1b89b1e1c8f1fd2e92d25c5fb
artemiszuk <68847756+artemiszuk@users.noreply.github.com>
2021-01-27 15:48:36 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
-git clone https://github.com/artemiszuk/aria2c-ariang
-cd aria2c-ariang
-if [ -f .env ]; then
-    echo ".env file found, sourcing it"
-	set -o allexport
-	source .env
-	set +o allexport
-fi
-mkdir downloads
-export PATH="$(cat downloads)"
-
+git clone https://github.com/artemiszuk/TorrentBotWebsite
+cd TorrentBotWebsite
 if [[ -n $RCLONE_CONFIG && -n $RCLONE_DESTINATION ]]; then
 	echo "Rclone config detected"
 	echo -e "[DRIVE]\n$RCLONE_CONFIG" > rclone.conf
-	echo "on-download-stop=./delete.sh" >> aria2c.conf
 	echo "on-download-complete=./on-complete.sh" >> aria2c.conf
-	chmod +x delete.sh
 	chmod +x on-complete.sh
 fi
 
