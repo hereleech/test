8cea87e568067921252fb3acc9da31cdde53e8a3
artemiszuk <68847756+artemiszuk@users.noreply.github.com>
2021-01-27 13:38:06 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ git clone https://github.com/artemiszuk/TorToolkit-Telegram
 cd TorToolkit-Telegram
 python3 -m venv venv
 source venv/bin/activate
+pip3 install -r requirements.txt
 python3 -m tortoolkit
