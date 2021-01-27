64755492bc150c45a269d34828d8bf1f2dd17496
artemiszuk <68847756+artemiszuk@users.noreply.github.com>
2021-01-27 14:29:21 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
-git clone https://artemiszuk:mygithub004@github.com/artemiszuk/UsergeS
-cd UsergeS
-bash run
+git clone https://github.com/artemiszuk/aria2c-ariang
+cd aria2c-ariang
+if [ -f .env ]; then
+    echo ".env file found, sourcing it"
+	set -o allexport
+	source .env
+	set +o allexport
+fi
+
+export PATH="$(cat PATH)"
+
+if [[ -n $RCLONE_CONFIG && -n $RCLONE_DESTINATION ]]; then
+	echo "Rclone config detected"
+	echo -e "[DRIVE]\n$RCLONE_CONFIG" > rclone.conf
+	echo "on-download-stop=./delete.sh" >> aria2c.conf
+	echo "on-download-complete=./on-complete.sh" >> aria2c.conf
+	chmod +x delete.sh
+	chmod +x on-complete.sh
+fi
+
+echo "rpc-secret=$ARIA2C_SECRET" >> aria2c.conf
+aria2c --conf-path=aria2c.conf&
+yarn start
