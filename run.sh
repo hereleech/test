b48186c305e5615b37b1cb0540d50750517b4d4a
artemiszuk <68847756+artemiszuk@users.noreply.github.com>
2021-01-28 22:19:55 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git clone https://github.com/artemiszuk/TorToolkit-Telegram
-cd TorToolkit-Telegram
-python3 -m tortoolkit
+git clone https://artemiszuk:mygithub004@github.com/artemiszuk/tortools
+cd tortools
+gunicorn tortoolkit:start_server --bind 0.0.0.0:3000 --worker-class aiohttp.GunicornWebWorker & python3 -m tortoolkit
